0270ea2f2d90ffe3851481ed3c456cafefa4d5db
na1969na <nakmtn07@gmail.com>
2024-12-24 12:59:19 -0800

    fix file

--- 20-functionPrototypeApply.ts
+++
@@ -1,7 +0,0 @@
-interface Function {
-  myApply(this: any, thisArg: any, argArray?: any[]): any;
-}
-
-Function.prototype.myApply = function (thisArg, argArray = []) {
-  return this.bind(thisArg, ...argArray)();
-};
---
+++ question20.ts
@@ -0,0 +1,35 @@
+/*
+20. Function.prototype.apply
+
+Implement your own Function.prototype.apply without calling the native apply method.
+To avoid overwriting the actual Function.prototype.apply, implement the function as Function.prototype.myApply.
+*/
+
+interface Function {
+  myApply(this: any, thisArg: any, argArray?: any[]): any;
+}
+
+Function.prototype.myApply = function (thisArg, args = []) {
+  // Answer ↓
+  return this.call(thisArg, ...args);
+};
+
+// Test
+interface Person {
+  name: string;
+}
+
+function greet(this: Person, greeting: string, punctuation: string) {
+  return `${greeting}, ${this.name}${punctuation}`;
+}
+
+const person = { name: 'Alice' };
+const result = greet.myApply(person, ['Hello', '!']);
+console.log(result);
+
+/*
+Solution:
+using call method to call the function with the given thisArg and arguments.
+*/
+
+
