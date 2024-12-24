bccdbf527f9b9a5efad87daf8e9b8b025a56c8ae
na1969na <nakmtn07@gmail.com>
2024-12-23 22:47:19 -0800

    add file

---
+++ 20-functionPrototypeApply.ts
@@ -0,0 +1,7 @@
+interface Function {
+  myApply(this: any, thisArg: any, argArray?: any[]): any;
+}
+
+Function.prototype.myApply = function (thisArg, argArray = []) {
+  return this.bind(thisArg, ...argArray)();
+};
